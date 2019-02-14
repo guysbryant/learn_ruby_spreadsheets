548dd8f9a3725175fcb3067709782ba22f7e9beb
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-14 06:31:29 -0500

    add needed require rubyXL

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,9 @@
 require "learn_ruby_spreadsheets/version"
 require "pry"
+require 'rubyXL'
 require 'rubyXL/convenience_methods'
 
+require_relative "./learn_ruby_spreadsheets/spreadsheets.rb"
 module LearnRubySpreadsheets
   class Error < StandardError; end
   # Your code goes here...
