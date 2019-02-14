91886d7db907822cabec1c6fe0693b2d721f9b72
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-14 06:18:22 -0500

    add require rubyXL/convenience_metods supposedly ram heavy

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 require "learn_ruby_spreadsheets/version"
 require "pry"
+require 'rubyXL/convenience_methods'
+
 module LearnRubySpreadsheets
   class Error < StandardError; end
   # Your code goes here...
