1c4240959602912f73944797456d131b35322c76
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-14 05:42:59 -0500

    setup with bundle gem learn_ruby_spreadsheets

---
+++ lib/learn_ruby_spreadsheets.rb
@@ -0,0 +1,6 @@
+require "learn_ruby_spreadsheets/version"
+
+module LearnRubySpreadsheets
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/learn_ruby_spreadsheets/version.rb
@@ -0,0 +1,3 @@
+module LearnRubySpreadsheets
+  VERSION = "0.1.0"
+end
