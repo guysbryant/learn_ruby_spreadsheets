65935e51ca39f3e5d8faf0d7bf01d7736f65f3b6
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-15 07:13:05 -0500

    comments for reading in workbooks, worksheets, rows, cells, and cell values.

--- lib/learn_ruby_spreadsheets/spreadsheets.rb
+++ lib/learn_ruby_spreadsheets/spreadsheets.rb
@@ -1,3 +1,22 @@
+=begin
+    Working with rubyXL
+    workbook = RubyXL::Parser.parse("path/to/xlsx")
+
+    work_sheet = workbook[0]
+    sheetByName = workbook['sheet_name'] this will find matching sheet
+
+    row1 = work_sheet[0]
+    workbook[0][0]
+
+    cell1 = row1[0]
+    workbook[0][0][0]
+    work_sheet[0][0]
+
+    cell_value = cell1.value
+=end
+rescue => exception
+    
+end
 class LearnRubySpreadsheets::SpreadSheets
 
     def initialize
