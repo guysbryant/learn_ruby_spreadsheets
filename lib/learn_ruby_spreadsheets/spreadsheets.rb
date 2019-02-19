029e7b8aba391939f5e0413f9e23cc0e0d20e7e1
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-19 08:20:40 -0500

    Save workbook with changed cell value

--- lib/learn_ruby_spreadsheets/spreadsheets.rb
+++ lib/learn_ruby_spreadsheets/spreadsheets.rb
@@ -1,6 +1,7 @@
 =begin
     Working with rubyXL
     workbook = RubyXL::Parser.parse("path/to/xlsx")
+    workbook.write("path/to/desired/Excel/file.xlsx")
 
     work_sheet = workbook[0]
     sheetByName = workbook['sheet_name'] this will find matching sheet
@@ -12,7 +13,9 @@
     workbook[0][0][0]
     work_sheet[0][0]
 
-    cell_value = cell1.value
+    cell_value = cell1.value to read the cell value
+    cell_value.change_contents("new value") to change the cell value
+    cell_value.change_contents("new value", cell_value.formula) to preserve the formula
 
     row.cells returns array-like object of all cells
     row.cells.each lets me itterate over all the cells to pull out values
